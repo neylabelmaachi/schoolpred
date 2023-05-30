bff3e52ffb5502ba8acf111d79f2d7dec6028981
Andrii Gegliuk <andrii.gegliuk@gmail.com>
2023-05-30 14:39:19 +0900

    title and description added to the home page

--- streamlit/andriigegliuk_dropout_streamlit.py
+++ streamlit/andriigegliuk_dropout_streamlit.py
@@ -14,8 +14,11 @@ page = st.sidebar.selectbox(
 )
 
 if page == '🏠 Home':
-    st.title('Primary School Dropout Predictor')
-    st.write("Please upload a CSV file with classroom data to predict the probability of student dropout.")
+    st.title('Primary school dropout prediction tool')
+    st.write("This tool has been built in order to prevent primary school dropout in high-risk rural regions of Morocco.")
+    st.write("""Our prediction model has been trained on the following research dataset:
+            Data for Development Initiative. (2019). Morocco CCT Education (Version 1.0)
+            [Data set]. Redivis. https://redivis.com/datasets/11xy-bb1z6q7ap?v=1.0""")
 
     data = {
     'province': ['Khenifra', 'Taroudant', 'Taourirt', 'Ouarzazate', 'Azilal',
