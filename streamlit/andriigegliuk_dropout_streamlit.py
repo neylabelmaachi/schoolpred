cc0b284c547ae31200710706ea26c80a70701f2a
Andrii Gegliuk <120349975+AnriiGegliuk@users.noreply.github.com>
2023-05-31 13:53:58 +0900

    andriigegliuk_dropout_streamlit.py

--- streamlit/andriigegliuk_dropout_streamlit.py
+++ streamlit/andriigegliuk_dropout_streamlit.py
@@ -26,16 +26,14 @@ if page == '🏠 Home':
 
 
 
-    st.title('Primary school dropout prediction tool')
+	st.title('Primary School Dropout Predictor')
     st.write("This tool has been built in order to prevent primary school dropout in high-risk rural regions of Morocco.")
     st.write("""Our prediction model has been trained on the following research dataset:
             Data for Development Initiative. (2019). Morocco CCT Education (Version 1.0)
-            [Data set]. Redivis. https://redivis.com/datasets/11xy-bb1z6q7ap?v=1.0""")
+            [Data set](https://redivis.com/datasets/11xy-bb1z6q7ap?v=1.0)""")
 
-
-
-    st.title('Primary School Dropout Predictor')
-    st.write("Please upload a CSV file with classroom data to predict the probability of student dropout.")
+    st.title('Identifying and preventing risks')
+    st.write("Please upload a CSV file with classroom data or manually input individual student data to predict the probability of school dropout.")
 
 
 
